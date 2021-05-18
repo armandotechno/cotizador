b213dc3ed293b78730d5608ec80670e201b7881b
Armando <armandotechno12@gmail.com>
2021-05-18 15:08:08 -0400

    Finalizando los styled components del formulario

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ const ContenedorHeader = styled.header`
 `;
 
 const TextoHeader = styled.h1`
-    font-size 2rem;
+    font-size: 2rem;
     margin:0;
     font-family: 'Slabo 27px', serif;
     text-align: center;
